1c99681f0bf8bdcf70bf9e6fe0a8d2ff45769872
Praveen_10492 <Praveen.Dhananjaya@dialog.lk>
2024-12-30 21:58:10 +0530

    update git action

--- Aggregator_Service/servie.py
+++ Aggregator_Service/servie.py
@@ -6,7 +6,7 @@ import os
 # MongoDB client setup
 MONGO_URI = os.getenv("MONGO_URI",
                       "mongodb+srv://healthsync.qntlu.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=HealthSync")
-MONGO_CERT_PATH = os.getenv("MONGO_CERT_PATH", "../Aggregator Service/X509-cert-8433791428290760769.pem")
+MONGO_CERT_PATH = os.getenv("MONGO_CERT_PATH", "/X509-cert-8433791428290760769.pem")
 DB_NAME = os.getenv("DB_NAME", "MediTrack")
 
 # Ensure the PEM file exists
