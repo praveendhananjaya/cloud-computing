119b419bbbb2f3c6eb0f4148dea319cb8e4b10d1
Praveen_10492 <Praveen.Dhananjaya@dialog.lk>
2025-01-01 23:08:36 +0530

    update ingress

--- Appointment_Sheduling_Service/service.py
+++ Appointment_Sheduling_Service/service.py
@@ -131,4 +131,4 @@ def get_doctor_availability(doctor_name):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=80)
--- Patient_Record_Service/service.py
+++ Patient_Record_Service/service.py
@@ -62,4 +62,4 @@ def get_patient(phone_number):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
